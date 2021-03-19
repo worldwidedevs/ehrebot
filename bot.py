8ebc9df91ba9c19404311c4d7062a232d0f8bc2b
berrysauce <contact@berrysauce.me>
2021-03-19 16:40:18 +0100

    Initial Release + Fix für Issue #2

--- bot.py
+++ bot.py
@@ -1,119 +1,22 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import os
 from random import randint
-import json
+import os
 from datetime import datetime
+from utils import database
 
 description = "Ein Bot der eine virtuelle Bank simuliert."
-bot = commands.Bot(command_prefix='.', description=description)
+bot = commands.Bot(command_prefix=".", description=description)
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+botOwner = ["215080717560971264", "405843581534994433"]
 
-# Database setup
-class UserDB(object):
-    def __init__(self , location):
-        self.location = os.path.expanduser(location)
-        self.load(self.location)
+udb = database.UserDB("user.db")
+cdb = database.ClaimDB("claim.db")
+fdb = database.FlipDB("flip.db")
 
-    def load(self , location):
-       if os.path.exists(location):
-           self._load()
-       else:
-            self.db = {}
-       return True
-
-    def _load(self):
-        self.db = json.load(open(self.location , "r"))
-
-    def dumpdb(self):
-        try:
-            json.dump(self.db , open(self.location, "w+"))
-            return True
-        except:
-            return False
-
-    def set(self , key , value):
-        try:
-            self.db[str(key)] = value
-            self.dumpdb()
-        except Exception as e:
-            print("[X] Error Saving Values to Database : " + str(e))
-            return False
-
-    def get(self , key):
-        try:
-            return self.db[key]
-        except KeyError:
-            print("No Value Can Be Found for " + str(key))
-            return False
-
-    def delete(self , key):
-        if not key in self.db:
-            return False
-        del self.db[key]
-        self.dumpdb()
-        return True
-    
-    def resetdb(self):
-        self.db={}
-        self.dumpdb()
-        return True
-	
-udb = UserDB("user.db")
-
-class ClaimDB(object):
-    def __init__(self , location):
-        self.location = os.path.expanduser(location)
-        self.load(self.location)
-
-    def load(self , location):
-       if os.path.exists(location):
-           self._load()
-       else:
-            self.db = {}
-       return True
-
-    def _load(self):
-        self.db = json.load(open(self.location , "r"))
-
-    def dumpdb(self):
-        try:
-            json.dump(self.db , open(self.location, "w+"))
-            return True
-        except:
-            return False
-
-    def set(self , key , value):
-        try:
-            self.db[str(key)] = value
-            self.dumpdb()
-        except Exception as e:
-            print("[X] Error Saving Values to Database : " + str(e))
-            return False
-
-    def get(self , key):
-        try:
-            return self.db[key]
-        except KeyError:
-            print("No Value Can Be Found for " + str(key))
-            return False
-
-    def delete(self , key):
-        if not key in self.db:
-            return False
-        del self.db[key]
-        self.dumpdb()
-        return True
-    
-    def resetdb(self):
-        self.db={}
-        self.dumpdb()
-        return True
-	
-cdb = ClaimDB("claim.db")
 
 @bot.event
 async def on_ready():
@@ -131,37 +34,49 @@ async def on_ready():
   await bot.change_presence(activity=discord.Game(name="on " + str(guild_count) + " servers | .help"))
   
   print("Bot is in " + str(guild_count) + " guilds")
+
     
 @bot.command(description="Testet den Ping des Bots", help="Testet den Ping des Bots")
 async def ping(ctx):
   await ctx.send("Pong! Bot latency: {0}".format(bot.latency))
-  
+
+
 @bot.command(description="Registriere dein Konto", help="Registriere dein Konto")
 async def signup(ctx):
   userBalance = udb.get(str(ctx.author.id))
-  if userBalance == False:
+  now = datetime.now()
+  datenow = now.strftime("%d-%m-%Y")
+  if userBalance == None:
     udb.set(str(ctx.author.id), 20)
     cdb.set(str(ctx.author.id), "none")
+    fdb.set(str(ctx.author.id), [0, datenow])
     await ctx.send("Dein Konto bei **ehrebank** wurde eröffnet. Als Willkommensgeschenk bekommst du `20 EHRE`!")
   else:
     await ctx.send("Du hast schon ein Konto eröffnet.")
-    
+
+
 @bot.command(description="Zeige deinen Kontostand an", help="Zeige deinen Kontostand an")
 async def balance(ctx):
   try:
     mentionedID = str(ctx.message.mentions[0].id)
     mentionedBalance = udb.get(mentionedID)
+    if mentionedBalance == None:
+      await ctx.send("Der Benutzer hat noch kein Konto und muss es erst mit `.signup` erstellen.")
+    else:
+      await ctx.send(f"Der Kontostand von {ctx.message.mentions[0]} bei der **ehrebank** ist: `{mentionedBalance} EHRE`")
   except:
-    await ctx.send("Du musst einen Benutzer erwähnen.")
-  if mentionedBalance == False:
-    await ctx.send("Der Benutzer hat noch kein Konto und muss es erst mit `.signup` erstellen.")
-  else:
-    await ctx.send(f"Der Kontostand von {ctx.message.mentions[0]} bei der **ehrebank** ist: `{mentionedBalance} EHRE`")
-    
+    mentionedID = str(ctx.author.id)
+    mentionedBalance = udb.get(mentionedID)
+    if mentionedBalance == None:
+      await ctx.send("Der Benutzer hat noch kein Konto und muss es erst mit `.signup` erstellen.")
+    else:
+      await ctx.send(f"Dein Kontostand bei der **ehrebank** ist: `{mentionedBalance} EHRE`")
+
+
 @bot.command(description="Sende EHRE", help="Sende EHRE")
 async def send(ctx, amount: int):
   senderBalance = udb.get(str(ctx.author.id))
-  if senderBalance == False:
+  if senderBalance == None:
     await ctx.send("Bitte eröffne zuerst ein Konto mit `.signup`!")
   else:
     if senderBalance < amount:
@@ -169,7 +84,7 @@ async def send(ctx, amount: int):
     else:
       receiverID = str(ctx.message.mentions[0].id)
       receiverBalance = udb.get(receiverID)
-      if receiverBalance == False:
+      if receiverBalance == None:
         await ctx.send("Der Empfänger hat noch kein Konto und muss es erst mit `.signup` erstellen.")
       else:
         if amount < 0:
@@ -182,17 +97,18 @@ async def send(ctx, amount: int):
           udb.set(senderID, newSenderBalance)
           await ctx.send(f"Transaktion fertig! `{amount} EHRE` wurde(n) zu `{ctx.message.mentions[0]}` gesendet.")
           await ctx.message.mentions[0].send(f"`{amount} EHRE` wurde(n) dir von `{ctx.author}` gesendet.")
-          
+
+
 @bot.command(description="Gebe EHRE", help="Gebe EHRE (kann nur der Bot Besitzer)")
 async def give(ctx, amount: int):
-  if str(ctx.author.id) == "215080717560971264":
+  if str(ctx.author.id) in botOwner:
     senderBalance = udb.get(str(ctx.author.id))
-    if senderBalance == False:
+    if senderBalance == None:
       await ctx.send("Bitte eröffne zuerst ein Konto mit `>signup`!")
     else:
       receiverID = str(ctx.message.mentions[0].id)
       receiverBalance = udb.get(receiverID)
-      if receiverBalance == False:
+      if receiverBalance == None:
         await ctx.send("Der Empfänger hat noch kein Konto und muss es erst mit `.signup` erstellen.")
       else:
         if amount < 0:
@@ -205,7 +121,8 @@ async def give(ctx, amount: int):
           await ctx.message.mentions[0].send(f"`{amount} EHRE` wurde(n) dir gegeben.")
   else:
     await ctx.send("Du must der Bot Besitzer sein um diese Aktion ausführen zu können.")
-    
+
+
 @bot.command(description="Claime täglich EHRE", help="Claime täglich EHRE")
 async def claim(ctx):      
   userBalance = udb.get(str(ctx.author.id))
@@ -231,10 +148,12 @@ async def claim(ctx):
       await ctx.send("Du hast `5 EHRE` geclaimt!")
     else:
       await ctx.send("Du kannst erst morgen wieder `EHRE` claimen, alla!")
-      
+
+
 @bot.command(description="Zeige die aktuelle Shopseite", help="Zeige die aktuelle Shopseite")
 async def shop(ctx):
   await ctx.send("**EHRE SHOP** \n`50 EHRE` Ritter \n`100 EHRE` Adel \n`500 EHRE` König \n`1000 EHRE` Gottheit \n \n Schreibe `.buy [Name des Items]` um etwas zu kaufen")
+
 
 @bot.command(description="Kaufe ein Item aus dem Shop", help="Kaufe ein Item aus dem Shop")
 async def buy(ctx, item: str):
@@ -251,7 +170,7 @@ async def buy(ctx, item: str):
     else:
       price = 50
       balance = udb.get(str(ctx.author.id))
-      if balance == False:
+      if balance == None:
         await ctx.send("Bitte eröffne zuerst ein Konto mit `.signup`!")
       else:
         if balance < price:
@@ -268,7 +187,7 @@ async def buy(ctx, item: str):
     else:
       price = 100
       balance = udb.get(str(ctx.author.id))
-      if balance == False:
+      if balance == None:
         await ctx.send("Bitte eröffne zuerst ein Konto mit `.signup`!")
       else:
         if balance < price:
@@ -285,7 +204,7 @@ async def buy(ctx, item: str):
     else:
       price = 500
       balance = udb.get(str(ctx.author.id))
-      if balance == False:
+      if balance == None:
         await ctx.send("Bitte eröffne zuerst ein Konto mit `.signup`!")
       else:
         if balance < price:
@@ -302,7 +221,7 @@ async def buy(ctx, item: str):
     else:
       price = 1000
       balance = udb.get(str(ctx.author.id))
-      if balance == False:
+      if balance == None:
         await ctx.send("Bitte eröffne zuerst ein Konto mit `.signup`!")
       else:
         if balance < price:
@@ -315,5 +234,69 @@ async def buy(ctx, item: str):
           await ctx.send("Du hast `Gottheit` für `1000 EHRE` gekauft! GÖTTLICHE EHRE!")
   else:
     await ctx.send("Sorry, aber dieses Item gibt es nicht.")
+
+
+@bot.command(description="Wirf eine Münze", help="Wirf eine Münze")
+async def coinflip(ctx, side: str, amount: int):
+  side = side.lower()
+  balance = udb.get(str(ctx.author.id))
+  flips = fdb.get(str(ctx.author.id))
+  now = datetime.now()
+  datenow = now.strftime("%d-%m-%Y")
+
+  if flips == False:
+    fdb.set(str(ctx.author.id), [0, datenow])
+
+  if flips[0] >= 5 and flips[1] == datenow:
+    await ctx.send("Du hast dein tägliches Flip-Limit erreicht!")
+  else:
+    if flips[1] != datenow:
+      fdb.set(str(ctx.author.id), [0,datenow])
+
+    if balance == None:
+      await ctx.send("Bitte eröffne zuerst ein Konto mit `.signup`!")
+    else:
+      if balance < amount:
+        await ctx.send("Dein Kontostand kann diesen Betrag nicht decken.")
+      elif side != "heads" and side != "tails":
+        await ctx.send("`{0}` ist keine gültige Seite. Probiere `heads` oder `tails`.".format(side))
+      else:
+        newFlips = []
+        newFlips.append(fdb.get(str(ctx.author.id))[0] + 1)
+        newFlips.append(datenow)
+        fdb.set(str(ctx.author.id), newFlips)
+        randSide = randint(1,2)
+        sidePercentage = randint(1,50)
+        if sidePercentage == 1:
+          randSide = "side"
+        elif randSide == 1:
+          randSide = "heads"
+        elif randSide == 2:
+          randSide = "tails"
+        if side == randSide:
+          await ctx.send("Du hast `{0}` geworfen! Du gewinnst `{1} EHRE`!".format(randSide, amount))
+          udb.set(str(ctx.author.id),balance+amount)
+        else:
+          await ctx.send("Du hast `{0}` geworfen! Du verlierst `{1} EHRE`!".format(randSide, amount))
+          udb.set(str(ctx.author.id), balance-amount)
+
+
+@bot.command(description="Limits zurücksetzen", help="Limits zurücksetzen (kann nur der Bot Besitzer)")
+async def reset(ctx, type: str):
+  if str(ctx.author.id) in botOwner:
+    receiverID = str(ctx.message.mentions[0].id)
+    now = datetime.now()
+    datenow = now.strftime("%d-%m-%Y")
+
+    if type == "claim":
+      cdb.set(str(receiverID), "none")
+      await ctx.send("`{0}` limit wurde für {1} zurückgesetzt.".format(type, ctx.message.mentions[0]))
+    elif type == "coinflip":
+      fdb.set(str(receiverID), [0,datenow])
+      await ctx.send("`{0}` limit wurde für {1} zurückgesetzt.".format(type, ctx.message.mentions[0]))
+    else:
+      await ctx.send("`{0}` existiert nicht.".format(type))
+  else:
+    await ctx.send("Du must der Bot Besitzer sein um diese Aktion ausführen zu können.")
         
 bot.run(DISCORD_TOKEN)
